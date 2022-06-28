fe42d68c0ada225a7071c0e8ad6a4de947ba9813
Kubrianity <kubraaozerenn@gmail.com>
2022-06-28 13:39:18 +0300

    Set the current time as progressbar is dragged

--- scripts.js
+++ scripts.js
@@ -24,10 +24,20 @@ const handleProgress = () => {
     const percent = (video.currentTime / video.duration) * 100 ;
     progressBar.style.flexBasis = `${percent}%`;
 }
+const scrub = (e) => {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateBtn)
 video.addEventListener("pause", updateBtn)
 video.addEventListener("timeupdate", handleProgress) //Change progressbar as current time is updated
 toggle.addEventListener("click", togglePlay);
-skipButtons.forEach(button => button.addEventListener("click", skip))
-ranges.forEach(range => range.addEventListener("input", handleRange))
+skipButtons.forEach(button => button.addEventListener("click", skip));
+ranges.forEach(range => range.addEventListener("input", handleRange));
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e)); //Works only mouse is down
+progress.addEventListener("mousedown", () => mousedown = true);
+progress.addEventListener("mouseup", () => mousedown = false);
